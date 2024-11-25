238922db98dc7d65d613b05876fd04c440a577ba
Praveenkumar21v <mr.chandu.22@gmail.com>
2024-11-25 18:32:52 +0530

    first commit

--- client/src/pages/CheckPasswordPage.js
+++ client/src/pages/CheckPasswordPage.js
@@ -36,7 +36,7 @@ const CheckPasswordPage = () => {
     e.preventDefault();
     e.stopPropagation();
 
-    const URL = `${process.env.REACT_APP_BACKEND_URL}/api/password`;
+    const URL = `${process.env.REACT_APP_BACKEND_URL || process.env.REACT_APP_PRODUCTION_BACKEND_URL}/api/password`;
 
     try {
       const response = await axios({
